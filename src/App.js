9441045db7334782f0a35143e4ed5743e72fc047
RubelMollah-RM <rubelmollah9311@gmail.com>
2022-11-16 21:13:59 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/Home/About';
+import Appointment from './pages/Home/Appointment';
+import ContactUs from './pages/Home/ContactUs';
+import Home from './pages/Home/Home';
+import Login from './pages/Home/Login';
+import Reviews from './pages/Home/Reviews';
+import Navbar from './pages/Shard/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/appointment' element={<Appointment />}></Route>
+        <Route path='/contact' element={<ContactUs />}></Route>
+        <Route path='/reviews' element={<Reviews />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
